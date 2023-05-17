f20e15beb823df1d59330342f6b4dd4887236026
Rami Karim <rami.karim95@gmail.com>
2023-05-17 15:48:02 +0200

    Added rate limiter to server

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/rka0917/Abios-HTTPServer
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/didip/tollbooth/v7 v7.0.1 // indirect
+	github.com/go-pkgz/expirable-cache v0.1.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/didip/tollbooth/v7"
 	"github.com/joho/godotenv"
 	"github.com/rka0917/Abios-HTTPServer/handlers"
 )
 
 func main() {
 	godotenv.Load(".env")
-	http.HandleFunc("/series/live", handlers.LiveSeriesHandler)
-	http.HandleFunc("/players/live", handlers.LivePlayersHandler)
-	http.HandleFunc("/teams/live", handlers.LiveTeamsHandler)
+	lmt := tollbooth.NewLimiter(5, nil)
+	lmt.SetMessage("You have reached maximum request limit")
+	http.Handle("/series/live", tollbooth.LimitFuncHandler(lmt, handlers.LiveSeriesHandler))
+	http.Handle("/players/live", tollbooth.LimitFuncHandler(lmt, handlers.LivePlayersHandler))
+	http.Handle("/teams/live", tollbooth.LimitFuncHandler(lmt, handlers.LiveTeamsHandler))
 
 	log.Println("Listening on localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
